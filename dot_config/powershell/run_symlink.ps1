79b59f20e221ebbc191749c55d7f76803a169d06
Joel Bennett <Jaykul@HuddledMasses.org>
2023-03-11 16:57:07 -0500

    More cross-platform

--- dot_config/powershell/run_symlink.ps1
+++ dot_config/powershell/run_symlink.ps1
@@ -1,10 +1,17 @@
+#!/usr/bin/env pwsh
+Install-Module PowerShellGet -AllowPrerelease -Force
+Set-PSResourceRepository -Name PSGallery -Trusted
 Install-PSResource EzTheme, Theme.PowerShell, Theme.PSReadLine, Theme.Terminal, Theme.PSStyle -Prerelease -WarningAction Ignore
+
+# On Windows, I need an extra copy in the WindowsPowerShell folder
 if ($Env:OneDriveCommercial) {
-    # The script will run in the current directory
-    # It won't be copied there, so we have to use PWD not PSScriptRoot
-    Get-ChildItem $PWD\*.ps1 | ForEach-Object {
+    Get-ChildItem profile.ps1 | ForEach-Object {
         # Put each file in both the WindowsPowerShell and PowerShell folders
         Copy-Item $_.FullName -Destination "$Env:OneDriveCommercial\Documents\PowerShell\$($_.Name)"
         Copy-Item $_.FullName -Destination "$Env:OneDriveCommercial\Documents\WindowsPowerShell\$($_.Name)"
     }
+}
+
+if ($Profile.CurrentUserAllHosts -ne (Convert-Path profile.ps1)) {
+    Copy-Item profile.ps1 $Profile.CurrentUserAllHosts
 }
