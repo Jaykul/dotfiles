7945c8246eaf72b682baceff50588ba142295f3a
Joel Bennett <Jaykul@HuddledMasses.org>
2023-09-13 22:45:39 -0400

    Clean up Initialize-ReadlineAI

--- dot_local/share/powershell/Scripts/Initialize-ReadlineAI.ps1
+++ dot_local/share/powershell/Scripts/Initialize-ReadlineAI.ps1
@@ -1,38 +1,31 @@
+[CmdletBinding()]
+param()
+
 if (!(Get-Module Conda)) {
     & "$PSScriptRoot/Initialize-Conda.ps1"
 }
 if (Get-Module Conda) {
-    if (!(Test-Path("~\Projects\Others\Codex-CLI\src\codex_query.py"))) {
+    if (!(Test-Path("$HOME\Projects\Others\Codex-CLI\src\codex_query.py"))) {
         Write-Warning "Cannot find codex_query.py - ReadlineAI not registered"
         return
     }
 
-    # this function takes the input from the buffer and passes it to codex_query.py
-    function create_completion() {
-        param (
-            [Parameter (Mandatory = $true)] [string] $buffer
-        )
-
-        Write-Progress -Id 1 -Activity "Codex query"
-        $output = $buffer | python "~\Projects\Others\Codex-CLI\src\codex_query.py"
-        Write-Progress -Id 1 -Activity "Codex query" -Completed
-
-        return $output
-    }
-
+    # this takes the input from the buffer and passes it to codex_query.py
     Set-PSReadLineKeyHandler -Key Ctrl+g `
         -BriefDescription ReadlineAI `
         -LongDescription "Calls ChatGPT with the current buffer" `
         -ScriptBlock {
         param($key, $arg)
 
-        $line = $null
+        $buffer = $null
         $cursor = $null
 
-        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$buffer, [ref]$cursor)
 
         # get response from create_completion function
-        $output = create_completion($line)
+        Write-Progress -Id 1 -Activity "Codex query"
+        $output = $buffer | python "$HOME\Projects\Others\Codex-CLI\src\codex_query.py"
+        Write-Progress -Id 1 -Activity "Codex query" -Completed
 
         # check if output is not null
         if ($output -ne $null) {
@@ -42,6 +35,11 @@ if (Get-Module Conda) {
                     [Microsoft.PowerShell.PSConsoleReadLine]::Insert($str)
                 }
             }
+        } else {
+            [Microsoft.PowerShell.PSConsoleReadLine]::AddLine()
+            [Microsoft.PowerShell.PSConsoleReadLine]::Insert("# No response from GPT")
         }
     }
+} else {
+    Write-Warning "Conda not initialized. ReadlineAI not registered"
 }
