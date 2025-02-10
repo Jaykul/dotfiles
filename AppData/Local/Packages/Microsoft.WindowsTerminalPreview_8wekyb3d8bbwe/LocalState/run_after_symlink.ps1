be61d013ad227432db6a55cfaf296bfa36d6f0ac
Joel Bennett <joelbennett@loandepot.com>
2025-02-10 11:58:59 -0500

    Fix Windows Terminal settings sync

--- AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/run_after_symlink.ps1
+++ AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/run_after_symlink.ps1
@@ -2,7 +2,7 @@
 # profile hash: {{ include "profile.ps1" | sha256sum }}
 $ErrorView = 'DetailedView'
 
-$NonPreviewLocalState = $Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+$NonPreviewLocalState = Convert-Path "$Env:LocalAppData/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState"
 if (Test-Path $NonPreviewLocalState) {
     # Use the same stuff for non-Preview Terminal
     Get-ChildItem *.png, *.ico, *.avif, settings.json
